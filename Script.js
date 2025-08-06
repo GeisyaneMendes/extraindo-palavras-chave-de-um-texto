4031d38f90a4a8ee68373021b7d2a5a5683de64f
GeisyaneMendes <geisyane.gomes@escola.pr.gov.br>
2025-08-06 02:51:30 -0300

    .

--- Script.js
+++ Script.js
@@ -31,6 +31,21 @@ function processaTexto(texto) {
       }
   }
 }
+function contaFrequencias(palavras) {
+  let frequencias = {};
+
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
+    }
+  }
+
+  return frequencias;
+}
 
   console.log(frequencias);
 
