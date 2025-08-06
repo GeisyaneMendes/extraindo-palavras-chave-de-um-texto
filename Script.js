985480ae1e5c8fff92de15fdac53659c133ecaf9
GeisyaneMendes <geisyane.gomes@escola.pr.gov.br>
2025-08-06 03:56:40 -0300

    .

--- Script.js
+++ Script.js
@@ -27,7 +27,7 @@ palavras = tiraPalavrasRuins(palavras);
   function ordenaPalavra(p1, p2) {
     return frequencias[p2] - frequencias[p1];
   }
-  
+
   return ordenadas.slice(0, 10);
 }
 
@@ -43,6 +43,7 @@ function contaFrequencias(palavras) {
   }
     return frequencias;
 }
+
 function tiraPalavrasRuins(palavras) {
   const palavrasBoas = [];
   for (let palavra of palavras) {
